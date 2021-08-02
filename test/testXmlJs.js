e497c012daf747586d812f13c5f25998c0acd877
Lencho <lencho@qq.com>
2021-08-02 10:27:08 +0800

    测试xml转json

--- test/testXmlJs.js
+++ test/testXmlJs.js
@@ -4,4 +4,5 @@ const fs = require('fs');
 let convert = require('xml-js');
 let xml = fs.readFileSync(filePath,'utf-8');
 let result1 = convert.xml2json(xml, {compact: true, spaces: 4});
-console.log(result1);
+console.log(JSON.parse(result1));
+// console.log(result1);
